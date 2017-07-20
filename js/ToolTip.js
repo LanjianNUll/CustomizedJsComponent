ce995c5dabf04b04786876ba33bc66db2fac8022
NULLLanJian <765797511@qq.com>
2017-07-20 17:06:14 +0800

    mouseover提示框完成

--- js/ToolTip.js
+++ js/ToolTip.js
@@ -3,24 +3,22 @@
 	"use strict";
 	var NAME = "ToolTip",
 	DEFAULTS = {
-		showDirection:"top",
+		showDirection:"right",
 		css:{
-			contentWidth:300,
-			contentHeight:160,
-			titleWidth: 300,
+			contentWidth:200,
+			contentHeight:60,
+			titleWidth: 200,
 			titleHeight: 40,
-			arrowWidht:30,
-			titleBgColor:"rgba(0,0,0,0.5)",
+			arrowWidth:30,
+			titleBgColor:"rgba(0,0,0,0.8)",
 			titleFontColor:"rgba(256,256,256,1)",
 			contentFontColor:"rgba(256,256,256,1)",
-			contentBgColor:"rgba(0,0,0,0.2)",
-			arrowBgColor:"rgba(0,0,0,0.2)",
+			contentBgColor:"rgba(0,0,0,0.5)",
+			arrowBgColor:"rgba(0,0,0,0.5)",
 			boderRadius:5,
 		},
 		title:"这里是标题",
 		content:"这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容",
-		
-    	
     };
 	
 	var idIncrementer = 0;
@@ -42,38 +40,79 @@
     	var that       = this,
             $root      = that.$,
             eventSuffix    = '.' + NAME + '.' + that.id;
-            //鼠标移入、移出的事件响应
-            $root.on("mouseenter", function () {
-                console.log("test mouseenter");
-            }).on("mouseleave", function () {
-                console.log("test mouseleave");
-            });
             
-            CreateToolTipDiv(that);
-            
+            var contentDiv = CreateToolTipDiv(that);
+			console.log(that);
+           //鼠标移动在上面
+           	$root.mouseover(function(e){
+           		
+           		//这里注意的是 tooltip的高度是conetentdiv加上arrowdiv的高度，而标题是contentdiv的子元素
+				var toolTipDivW = that.options.css.contentWidth+that.options.css.arrowWidth;
+            	var toolTipDivH = that.options.css.contentHeight+that.options.css.arrowWidth;
+            	var targetWidth = $root.outerWidth();
+            	var targetHeight =$root.outerHeight();
+            	var positiontop;
+            	var positionLeft;
+            	if(that.options.showDirection == "top"){
+		    		positiontop = -toolTipDivH;
+				 	positionLeft = targetWidth/2-toolTipDivW/2;
+		    	}
+		    	else if(that.options.showDirection == "bottom"){
+		    		positiontop = targetHeight+that.options.css.arrowWidth+that.options.css.titleHeight;
+				 	positionLeft = targetWidth/2-toolTipDivW/2;
+		    	}
+		    	else if(that.options.showDirection == "left"){
+		    		positiontop = toolTipDivH/2;
+		    		positionLeft = -toolTipDivW;
+		    	}
+		    	else if(that.options.showDirection == "right"){
+		    		positiontop = toolTipDivH/2;
+		    		positionLeft = targetWidth+that.options.css.arrowWidth;
+    			}
+				
+				ChangeDivPositioon(contentDiv,positionLeft,positiontop);
+				contentDiv.appendTo($root);
+				
+			});
+			//鼠标离开
+			$root.mouseout(function(e){
+				//鼠标离开不显示
+				contentDiv.css({
+					"display":"none"
+				});
+			});
 	}
+    
+    function ChangeDivPositioon(contentDiv,left,top){
+    	contentDiv.css({
+    		"display":"",
+    		"top":top+"px",
+    		"left":left+"px"
+    	});
+    }
     
     function CreateToolTipDiv(that){
     	var titleDiv = CreateToolTipTitleDiv(that);
     	var contentDiv = CreateToolTipContentDiv(that);
     	var arrowDiv;
-    	if(that.options.showDirection == "top"){
+    	//这里的上下左右是指 整个 tooltip在指定原素的相对位置
+    	if(that.options.showDirection == "bottom"){
     		arrowDiv = CreateToolTipArrowUpDiv(that);
     	}
-    	else if(that.options.showDirection == "bottom"){
+    	else if(that.options.showDirection == "top"){
     		arrowDiv = CreateToolTipArrowDownDiv(that);
     	}
-    	else if(that.options.showDirection == "left"){
+    	else if(that.options.showDirection == "right"){
     		arrowDiv = CreateToolTipArrowLeftDiv(that);
     	}
-    	else if(that.options.showDirection == "right"){
+    	else if(that.options.showDirection == "left"){
     		arrowDiv = CreateToolTipArrowRightDiv(that);
     	}
     	
     	titleDiv.appendTo(contentDiv);
     	arrowDiv.appendTo(contentDiv);
+    	return contentDiv;
     	
-    	contentDiv.appendTo("body");
     }
     
     
@@ -86,6 +125,7 @@
     		boderRadius = that.options.css.boderRadius;
     	
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"top":-titleHeight+"px",
     		"left":"0"+"px",
@@ -111,8 +151,8 @@
     	var top = 200;
     	var left = 200;
     	
-    	
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"top":top+"px",
     		"left":left+"px",
@@ -129,7 +169,7 @@
     
     function CreateToolTipArrowUpDiv(that){
     	var div = $("<div>");
-    	var arrowWidth = that.options.css.arrowWidht,
+    	var arrowWidth = that.options.css.arrowWidth,
     		contentWidth = that.options.css.contentWidth,
     		contentHeight = that.options.css.contentHeight,
     		titleHeight = that.options.css.titleHeight,
@@ -139,6 +179,7 @@
     		top =  -titleHeight-arrowWidth;
     	
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"left":left+"px",
     		"top":top+"px",
@@ -153,7 +194,7 @@
     
     function CreateToolTipArrowDownDiv(that){
     	var div = $("<div>");
-    	var arrowWidth = that.options.css.arrowWidht,
+    	var arrowWidth = that.options.css.arrowWidth,
     		contentWidth = that.options.css.contentWidth,
     		contentHeight = that.options.css.contentHeight,
     		titleHeight = that.options.css.titleHeight,
@@ -161,6 +202,7 @@
     	var left = contentWidth/2,
     		top =  contentHeight;
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"left":left+"px",
     		"top":top+"px",
@@ -175,7 +217,7 @@
     
     function CreateToolTipArrowLeftDiv(that){
     	var div = $("<div>");
-    	var arrowWidth = that.options.css.arrowWidht,
+    	var arrowWidth = that.options.css.arrowWidth,
     		contentWidth = that.options.css.contentWidth,
     		contentHeight = that.options.css.contentHeight,
     		titleHeight = that.options.css.titleHeight,
@@ -184,6 +226,7 @@
     		top =  contentHeight/2 - arrowWidth/2*3;
     	
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"left":left+"px",
     		"top":top+"px",
@@ -198,7 +241,7 @@
     
     function CreateToolTipArrowRightDiv(that){
     	var div = $("<div>");
-    	var arrowWidth = that.options.css.arrowWidht,
+    	var arrowWidth = that.options.css.arrowWidth,
     		contentWidth = that.options.css.contentWidth,
     		contentHeight = that.options.css.contentHeight,
     		titleHeight = that.options.css.titleHeight,
@@ -206,6 +249,7 @@
     	var left = contentWidth,
     		top =  contentHeight/2 - arrowWidth/2*3;
     	div.css({
+    		"z-index":"99",
     		"position":"absolute",
     		"left":left+"px",
     		"top":top+"px",
--- js/testToolTipDemo.js
+++ js/testToolTipDemo.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
 	
-	var options = {
-		
-		
-		
+	var options1 = {
+		showDirection:"top",
+		title:"这里是标题",
+		content:"这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容",
 		
 	};
 	
+	var options2 = {
+		showDirection:"bottom",
+		title:"这里是标题",
+		content:"这里是是内容",
+		
+	};
 	
-	$("#ee").toolTip();
+	var options3= {
+		showDirection:"left",
+		title:"这里是标题",
+		content:"这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容",
+		
+	};
 	
+	var options4 = {
+		showDirection:"right",
+		title:"这里是标题",
+		content:"这里是内容这里是内容这里是内容这里是内容这里是内容这里是内容",
+		
+	};
+	
+	$("#e1").toolTip(options1);
+	$("#e2").toolTip(options2);
+	$("#e3").toolTip(options3);
+	$("#e4").toolTip(options4);
 });
